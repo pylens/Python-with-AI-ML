7f7c45360f4b3f5743a038d7190f0968280e0c65
pylens <pylens.inst@gmail.com>
2025-07-15 12:19:06 +0530

    Add files via upload

---
+++ Python Variables, Data Types, Type Conversion, and Size Details/welcome.py
@@ -0,0 +1,27 @@
+import sys
+
+print("welcome world")
+
+name = "hemanth is an AI/ML Engineer"
+age = 20
+salary = 8.9
+# name, age and salary are the name of the variable
+
+
+Name = "harish"
+
+print("Name") # a string is the content inside the "" or single ''
+
+print("Slary of the persion is", salary, "lacks")
+
+
+
+print()
+print("size of the name is : ", sys.getsizeof(name),"\nsize of the age is :", sys.getsizeof(age), "\nand ssize of the salary is : ",sys.getsizeof(salary))
+
+
+isAvailable = True
+
+# type
+print("\ntype of the name is : ", type(name),"\ntype of the age is :", type(age), "\nand type of the salary is : ",type(salary))
+
