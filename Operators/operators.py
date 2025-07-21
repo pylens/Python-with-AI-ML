317af104e3ac5832751d38e80aae591ea69d8f74
PyLens <pylens.inst@gmail.com>
2025-07-22 00:40:55 +0530

    added new script for comparission operators

---
+++ Operators/operators.py
@@ -0,0 +1,21 @@
+## comparission operators
+# it is used to compare the two oparends using an operator
+
+a = 10
+b = 200
+
+print(a==b) # it will return the False
+print(a!=b) # it returns True
+print(a>=b) 
+print(a<=b) 
+print("****************String comparission***********")
+## String comparsion
+# Note python is type senstive 
+name1 = "Hemanth"
+print(id(name1))
+name2 = "hemanth"
+print(id(name2)) # it will return the different id
+print(name1==name2)
+
+
+print(10 <= a < 100 > b)
